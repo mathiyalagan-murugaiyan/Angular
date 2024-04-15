a0619245e1ac724368981b86a1f8aafe53c82e0c
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2024-04-15 18:22:53 +0530

    color picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule,ReactiveFormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CommonModule, ReactiveFormsModule]
 })
 export class AppComponent {
 
---
+++ src/color-picker/color-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './color-picker.component.html',
+  styleUrl: './color-picker.component.css'
+})
+export class ColorPickerComponent {
+
+}
